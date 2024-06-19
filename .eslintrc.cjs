42e4d90604b2161cdecd95baa8e4906e0fc8542c
viperluan <viperluan@gmail.com>
2024-06-19 18:45:57 -0300

    adiciona configurações do eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,38 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'eslint-plugin-import-helpers'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'import-helpers/order-imports': [
+      'error',
+      {
+        "newlinesBetween": "always",
+        "groups": [
+          "/^next/",
+          "/^react/",
+          "module",
+          "/^@shared/",
+          "/^~hooks/",
+          [
+            "parent",
+            "sibling",
+            "index"
+          ],
+          "/\\.module\\.scss/"
+        ],
+        "alphabetize": {
+          "order": "asc",
+          "ignoreCase": true
+        }
+      }
+    ]
   },
 }
