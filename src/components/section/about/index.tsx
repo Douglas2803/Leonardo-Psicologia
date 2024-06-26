64b6377958a3094af16cd71d1215c45e88c4d48e
viperluan <viperluan@gmail.com>
2024-06-26 14:03:30 -0300

    altera justify center para between

--- src/components/section/about/index.tsx
+++ src/components/section/about/index.tsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <section id="about" className="md:bg-slate-400">
-      <div className="container md:flex justify-center">
+      <div className="container md:flex justify-between">
         <div className=" mb-10 md:pr-5 lg:max-w-5xl">
           <h2 className="hidden md:block md:pt-3 md:pb-16 md:text-4xl">Sobre</h2>
           <h3 className="text-4xl font-bold mb-5">Leonardo Buziki</h3>
