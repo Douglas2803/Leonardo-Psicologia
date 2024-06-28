6726395f022aa6f66831e96bdabff6b1d952fbb2
Douglas <douglasgia@hotmail.com>
2024-06-28 20:21:15 -0300

    Criação de componente Bullet

---
+++ src/components/section/timeline/bullet/index.tsx
@@ -0,0 +1,7 @@
+const Bullet = () => {
+  return (
+    <div className="before:content-[''] before:w-5 before:h-5 ml-[-14px] mt-[35px] before:absolute before:bg-black before:rounded-full before:border-[7px] before:border-white"></div>
+  );
+};
+
+export { Bullet };
