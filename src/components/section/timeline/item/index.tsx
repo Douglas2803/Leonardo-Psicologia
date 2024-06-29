2c40c9a9e0738e8ef9866dbe81b7ff2634b76051
Douglas <douglasgia@hotmail.com>
2024-06-29 08:55:47 -0300

    Ajuste de componente Item

--- src/components/section/timeline/item/index.tsx
+++ src/components/section/timeline/item/index.tsx
@@ -13,28 +13,38 @@ interface ItemProps {
 
 const Item = ({ title, subtitle, text, urlImage, alt }: ItemProps) => {
   return (
-    <div className="flex flex-row ">
-      <Line className="w-[10px] h-auto mr-5 bg-black md:hidden">
-        <Bullet />
-      </Line>
-      <div className="flex flex-col md:flex-row">
-        <div>
-          <div className="flex flex-col md:odd:flex-row even:flex-row-reverse justify-between gap-y-1 ">
-            <div className="md:w-[40%]  py-5">
-              <h2 className=" text-xl font-black even:text-green-500">{title}</h2>
-              <h3 className="text-lg font-bold">{subtitle}</h3>
-              <p>{text}</p>
-            </div>
-
-            <Line className="w-[2px] md:wx-[10%] h-auto mx-5 bg-black hidden md:block">
-              <Bullet />
-            </Line>
-
-            <img className="pb-5 md:w-[40%]" src={urlImage} alt={alt} />
+    <>
+      <div className="flex md:odd:text-right md:even:flex-row-reverse justify-between gap-y-1 ">
+        <Line className="w-[10px] h-auto mr-5 bg-black md:hidden">
+          <Bullet />
+        </Line>
+        <div className="flex flex-col md:hidden">
+          <div className="md:w-[40%]  py-5">
+            <h2 className=" text-xl font-black">{title}</h2>
+            <h3 className="text-lg font-bold">{subtitle}</h3>
+            <p>{text}</p>
           </div>
+
+          <Line className="w-[2px] md:wx-[10%] h-auto mx-5 bg-black hidden md:block">
+            <Bullet />
+          </Line>
+
+          <img className="pb-5 md:w-[40%]" src={urlImage} alt={alt} />
         </div>
+
+        <div className="hidden md:block md:w-[40%]  py-5">
+          <h2 className=" text-xl font-black">{title}</h2>
+          <h3 className="text-lg font-bold">{subtitle}</h3>
+          <p>{text}</p>
+        </div>
+
+        <Line className="hidden md:block w-[2px] md:wx-[10%] h-auto mx-5 bg-black">
+          <Bullet />
+        </Line>
+
+        <img className="hidden md:block pb-5 md:w-[40%]" src={urlImage} alt={alt} />
       </div>
-    </div>
+    </>
   );
 };
 
