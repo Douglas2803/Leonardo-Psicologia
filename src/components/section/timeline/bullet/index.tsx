85494970748ee13e83ff4ef8015fcaa2c9d6a4e4
Douglas <douglasgia@hotmail.com>
2024-06-29 08:56:17 -0300

    Ajuste de componente Bullet

--- src/components/section/timeline/bullet/index.tsx
+++ src/components/section/timeline/bullet/index.tsx
@@ -1,6 +1,6 @@
 const Bullet = () => {
   return (
-    <div className="before:content-[''] before:w-5 before:h-5 ml-[-14px] mt-[35px] before:absolute before:bg-black before:rounded-full before:border-[7px] before:border-white"></div>
+    <div className="before:content-[''] before:w-5 before:h-5 ml-[-14px] mt-[35px] before:absolute before:bg-black before:rounded-full before:border-[7px] before:border-white text-justify"></div>
   );
 };
 
