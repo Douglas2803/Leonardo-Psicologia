2f9e7d295e8e4a3a633706bca7e3d3307d35039b
Douglas <douglasgia@hotmail.com>
2024-06-30 15:28:24 -0300

    Adição de componente Title no App

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Footer } from './components/footer';
 import { Header } from './components/header';
 import { About } from './components/section/about';
 import { Contact } from './components/section/contact';
+import { Title } from './components/section/title';
 
 function App() {
   return (
     <>
       <Header />
+      <Title />
       <Contact />
       <About />
       <Footer />
